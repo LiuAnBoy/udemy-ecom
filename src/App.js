6cf46f5c69c46f393add71122f5768b705ba3382
Eric.LA Chen(陳律安) <eric.la_chen@ericlachen.htctaipei.htc.com.tw>
2020-05-19 19:13:23 +0800

    React Router Demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './component/homepage/homepage.component';
+// import HomePage from './component/homepage/homepage.component';
+
+const HomePage = () => {
+  
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicsList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
